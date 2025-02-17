3f002ae6365b3c2a32aea233e54130ef9cc29cd4
Ismael Canga <ismaelcm841@gmail.com>
2025-02-17 14:13:39 -0500

    OperadorAND

--- Operadores/src/main/java/OperadorAnd.java
+++ Operadores/src/main/java/OperadorAnd.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class OperadorAnd {
+    public static void main(String[] args) {
+        System.out.println("*** Operador AND ***");
+        boolean a = true, b = false;
+        //and (Regresa true solo si ambos valores son true)
+        var resultado = a && b;
+        System.out.println("resultado = " + resultado);
+    }
 }
